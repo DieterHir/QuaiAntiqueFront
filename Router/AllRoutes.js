8c344d72bd3c0ecda85fdf38c8356a1d4c6ee674
Dieter <dieter59136@live.fr>
2024-02-21 20:57:46 +0100

    Ajout système routage

---
+++ Router/AllRoutes.js
@@ -0,0 +1,9 @@
+import Route from "./Route.js";
+
+//Définir les routes ici
+export const allRoutes = [
+    new Route("/", "Accueil", "/pages/home.html"),
+];
+
+//Le titre s'affichera comme ceci : Route.titre - websiteName
+export const websiteName = "Quai Antique";
