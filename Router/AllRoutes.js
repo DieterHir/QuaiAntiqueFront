000bdb33538c9ced5d925bb595bdb7e2348f46f7
Dieter <dieter59136@live.fr>
2024-02-22 18:22:47 +0100

    ajout de la galerie

--- Router/AllRoutes.js
+++ Router/AllRoutes.js
@@ -3,6 +3,7 @@ import Route from "./Route.js";
 //Définir les routes ici
 export const allRoutes = [
     new Route("/", "Accueil", "/pages/home.html"),
+    new Route("/galerie", "Galerie", "/pages/galerie.html"),
 ];
 
 //Le titre s'affichera comme ceci : Route.titre - websiteName
