378120abbd7795d5f4201525b4896ab1cde6a8fe
Dieter <dieter59136@live.fr>
2024-02-23 18:44:16 +0100

    ajout de la page inscription

--- Router/AllRoutes.js
+++ Router/AllRoutes.js
@@ -6,6 +6,8 @@ export const allRoutes = [
     new Route("/galerie", "Galerie", "/pages/galerie.html"),
     new Route("/carte", "La Carte", "/pages/carte.html"),
     new Route("/signin", "Connexion", "/pages/auth/signin.html"),
+    new Route("/signup", "Inscription", "/pages/auth/signup.html"),
+    new Route("/account", "Mon Compte", "/pages/auth/account.html"),
 ];
 
 //Le titre s'affichera comme ceci : Route.titre - websiteName
