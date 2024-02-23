c93441537738910bb151d76f9a05ed356cc07973
Dieter <dieter59136@live.fr>
2024-02-23 18:09:04 +0100

    ajout de la carte

--- Router/AllRoutes.js
+++ Router/AllRoutes.js
@@ -4,6 +4,8 @@ import Route from "./Route.js";
 export const allRoutes = [
     new Route("/", "Accueil", "/pages/home.html"),
     new Route("/galerie", "Galerie", "/pages/galerie.html"),
+    new Route("/carte", "La Carte", "/pages/carte.html"),
+    new Route("/signin", "Connexion", "/pages/auth/signin.html"),
 ];
 
 //Le titre s'affichera comme ceci : Route.titre - websiteName
